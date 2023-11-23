aa8f7802b226d18614c2d15488746157b4eb8ffd
Erdem Sarili <erdem@cloudflare.com>
2023-11-23 20:29:12 +0100

    enable gopls by default

--- init.lua
+++ init.lua
@@ -396,9 +396,9 @@ end
 local function live_grep_git_root()
   local git_root = find_git_root()
   if git_root then
-    require('telescope.builtin').live_grep {
+    require('telescope.builtin').live_grep({
       search_dirs = { git_root },
-    }
+    })
   end
 end
 
@@ -585,7 +585,7 @@ require('mason-lspconfig').setup()
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
