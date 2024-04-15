246080b94168503151462e98b1163cd8e871f643
Erdem Sarili <erdem@cloudflare.com>
2024-04-15 13:20:41 +0200

    switch to nvim-neotest

--- lua/custom/plugins/test.lua
+++ lua/custom/plugins/test.lua
@@ -1,6 +1,14 @@
 return {
-  'klen/nvim-test',
-  config = function()
-    require('nvim-test').setup()
-  end,
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'rouge8/neotest-rust',
+  },
+  opts = {
+    adapters = {
+      'rouge8/neotest-rust',
+    },
+  },
 }
