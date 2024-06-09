f72babfd3a95378199747843b47fea30bee239fd
Erdem Sarili <erdem@cloudflare.com>
2024-06-09 12:59:24 +0200

    fix harpoon desc

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -6,6 +6,6 @@ return {
     local harpoon_mark = require 'harpoon.mark'
     vim.keymap.set('n', '<leader>h<leader>', harpoon_ui.toggle_quick_menu, { desc = 'Toogle [H]arpoon' })
     vim.keymap.set('n', '<leader>n', harpoon_ui.nav_next, { desc = '[N]ext file' })
-    vim.keymap.set('n', '<leader>ha', harpoon_mark.add_file, { desc = 'Toogle [H]arpoon' })
+    vim.keymap.set('n', '<leader>ha', harpoon_mark.add_file, { desc = '[H]arpoon [A]dd' })
   end,
 }
