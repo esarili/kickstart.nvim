9d167e9d2dfdc5fe5f4cadb85330e03cee9f89cc
Erdem Sarili <erdem@cloudflare.com>
2024-11-01 14:16:13 +0100

    change test config to neotest-rust

--- lua/custom/plugins/test.lua
+++ lua/custom/plugins/test.lua
@@ -1,14 +1,14 @@
 return {
-  'nvim-neotest/neotest',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'antoinemadec/FixCursorHold.nvim',
-    'nvim-treesitter/nvim-treesitter',
-    'rouge8/neotest-rust',
-  },
-  opts = {
-    adapters = {
-      'rouge8/neotest-rust',
-    },
-  },
+  -- 'nvim-neotest/neotest',
+  -- dependencies = {
+  --   'nvim-lua/plenary.nvim',
+  --   'antoinemadec/FixCursorHold.nvim',
+  --   'nvim-treesitter/nvim-treesitter',
+  --   'rouge8/neotest-rust',
+  -- },
+  -- opts = {
+  --   adapters = {
+  --     'rouge8/neotest-rust',
+  --   },
+  -- },
 }
