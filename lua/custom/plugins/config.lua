89371767be42f3359a3b938b687c983cac24c916
Erdem Sarili <erdem@cloudflare.com>
2026-03-14 23:06:03 +0100

    enable undo/spellcheck, disable eof

--- lua/custom/plugins/config.lua
+++ lua/custom/plugins/config.lua
@@ -3,4 +3,8 @@ vim.o.rnu = true
 vim.o.hlsearch = false
 vim.o.colorcolumn = '80,100'
 vim.o.scrolloff = 999
+vim.o.endofline = false
+vim.o.undofile = true
+vim.o.spelllang = 'en_gb'
+vim.o.spell = true
 return {}
