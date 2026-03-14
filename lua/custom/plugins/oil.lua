d611cf0715dc14bb6796d872f5690a4834518ef8
Erdem Sarili <erdem@cloudflare.com>
2026-03-14 23:08:24 +0100

    use ctrl+l for window navigation (disable oil binding)

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    keymaps = {
+      ['<C-l>'] = false,
+    },
+  },
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
