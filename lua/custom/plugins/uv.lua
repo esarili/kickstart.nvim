3e051823a3055104ca9216bc4d0f58f534739f37
Erdem Sarili <erdem@cloudflare.com>
2026-04-02 12:15:55 +0200

    install uv plugin

---
+++ lua/custom/plugins/uv.lua
@@ -0,0 +1,12 @@
+return {
+  'benomahony/uv.nvim',
+  -- ft = { python },
+  -- dependencies = {
+  --   'folke/snacks.nvim',
+  --   'nvim-telescope/telescope.nvim',
+  -- },
+  opts = {
+    picker_integration = true,
+  },
+}
+
