3ca37645f3f5ac1217e5dc826f7ada0a4c4c06df
Erdem Sarili <erdem@cloudflare.com>
2026-04-29 14:03:28 +0200

    update rustaceanvim to version ^9

--- lua/custom/plugins/rustacean.lua
+++ lua/custom/plugins/rustacean.lua
@@ -1,5 +1,10 @@
 return {
   'mrcjkb/rustaceanvim',
-  version = '^6', -- Recommended
-  lazy = false, -- This plugin is already lazy
+  -- To avoid being surprised by breaking changes,
+  -- I recommend you set a version range
+  version = '^9',
+  -- This plugin implements proper lazy-loading (see :h lua-plugin-lazy).
+  -- No need for lazy.nvim to lazy-load it.
+  lazy = false,
 }
+
